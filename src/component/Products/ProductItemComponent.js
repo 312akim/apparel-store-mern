706898dbfbbbae645362f65f4e408ed21a2e207f
Alex <312akim@gmail.com>
2020-08-18 02:05:07 -0500

    Main store page product layout

--- src/component/Products/ProductItemComponent.js
+++ src/component/Products/ProductItemComponent.js
@@ -4,8 +4,8 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Button } from 'reactstrap
 const ItemDisplay = ({item}) => {
     console.log(item.name)
     return (
-        <Card>
-            <CardImg top width="25%" src={item.images[0]} alt={item.name}/>
+        <Card className="itemCards">
+            <CardImg top width="20%" src={item.images[0]} alt={item.name}/>
             <CardBody>
                 <CardTitle>{item.name}</CardTitle>
             </CardBody>
