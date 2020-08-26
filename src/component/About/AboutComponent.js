caab117a08349c18be5ead9739bf9586cbfa72bc
Alex Kim <312akim@gmail.com>
2020-08-25 22:24:40 -0500

    About us CSS page setup

--- src/component/About/AboutComponent.js
+++ src/component/About/AboutComponent.js
@@ -3,7 +3,18 @@ import React from 'react';
 const AboutPage = () => {
     return (
         <div>
-            This is the about us page!
+            <div>This is the about us page!</div>
+            <div className="box1">
+                <div>Who are we?</div>
+                <div>Text for "Who are we?" Here</div>
+            </div>
+            <div className="box2">Image for "Who are we?"" here</div>
+            <div className="box3">Image for "Our Story here"</div>
+            <div className="box4">
+                <div>Our story</div>
+                <div>Text for "Our story" Here</div>
+            </div>
+
         </div>
     )
 }
