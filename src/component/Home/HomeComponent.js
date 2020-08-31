6ec43d6e1ebd6aa85f1ef6a04b3e4859256cf284
Alex Kim <312akim@gmail.com>
2020-08-31 04:32:58 -0500

    Main page setup

--- src/component/Home/HomeComponent.js
+++ src/component/Home/HomeComponent.js
@@ -3,7 +3,16 @@ import React from 'react';
 const HomePage = () => {
     return (
         <div>
-            This is the HomePage!! / Landing!!
+            <h1>This is the HomePage!! / Landing!!</h1>
+            <div>
+                {/* <video width="320" height="240">
+                    <source src="https://thumbs.gfycat.com/SerpentineAngryBoto-mobile.mp4" type="video/mp4"></source>
+                </video> */}
+                <button>To Store Button</button>
+            </div>
+            <div>
+                Fade in Featured Clothes display
+            </div>
         </div>
     )
 }
