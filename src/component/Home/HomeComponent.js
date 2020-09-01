248f053bab3382de217963aef94136a47b09c720
Alex <312akim@gmail.com>
2020-08-31 21:31:50 -0500

    Home Page Background Video

--- src/component/Home/HomeComponent.js
+++ src/component/Home/HomeComponent.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import StyledVideo from './HomeComponentStyles'
 
 const HomePage = () => {
+    const videoSource = "https://www.w3schools.com/tags/movie.mp4";
     return (
         <div>
             <h1>This is the HomePage!! / Landing!!</h1>
             <div>
-                {/* <video width="320" height="240">
-                    <source src="https://thumbs.gfycat.com/SerpentineAngryBoto-mobile.mp4" type="video/mp4"></source>
-                </video> */}
-                <button>To Store Button</button>
+                <StyledVideo autoPlay loop muted className="homeBackgroundVideo">
+                    <source src={videoSource} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </StyledVideo>
             </div>
             <div>
                 Fade in Featured Clothes display
---
+++ src/component/Home/HomeComponentStyles.js
@@ -0,0 +1,7 @@
+import styles from 'styled-components';
+
+const StyledVideo = styles.video`
+    height: 80vh;
+`;
+
+export default StyledVideo;
