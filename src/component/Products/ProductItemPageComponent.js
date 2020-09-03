2b4f46814673f890bd23bc57b581cab723412d8b
Alex <312akim@gmail.com>
2020-09-03 01:18:23 -0500

    Product Page format

--- src/component/Products/ProductItemPageComponent.js
+++ src/component/Products/ProductItemPageComponent.js
@@ -1,27 +1,37 @@
 import React from 'react';
+import { StyledPreviewImg, PreviewContainerDiv, MainProductImg } from './ProductPageStyles';
   
 const ProductPage = ({item}) => {
 
+    const displayChanger = () => {
+
+    }
+    
+    let displayedImage = item.images[0].src;
+
     const imageMap = item.images.map((image) => {
         return (
-            <img className="imagePreview" src={image.src}></img>
+            <StyledPreviewImg onClick={displayChanger()} src={image.src}></StyledPreviewImg>
         )
     })
 
     const Cart = () => {
         return (
-            <div className="">Add to cart button here</div>
+            <div>
+                <p>Price Here</p>
+                <div>Add to cart button here</div>
+            </div>
         )
     }
-    
 
     return (
         <div>
             <div>
-                <img src={item.images[0].src}></img>
+                <h2>{item.name}</h2>
+                <MainProductImg src={item.images[0].src}></MainProductImg>
                 <Cart />
             </div>
-            <div className="previewContainer">{imageMap}</div>
+            <PreviewContainerDiv>{imageMap}</PreviewContainerDiv>
         </div>
     )
 }
---
+++ src/component/Products/ProductPageStyles.js
@@ -0,0 +1,17 @@
+import styles from 'styled-components';
+
+export const StyledPreviewImg = styles.img`
+    display: inline-block;
+    max-height: 10rem;
+    width: auto;
+`;
+
+export const PreviewContainerDiv = styles.div`
+    margin: 1rem 20rem;
+    border: solid black 1px;
+`;
+
+export const MainProductImg = styles.img`
+    margin-top: 1rem;
+    max-height: 30rem;
+`;
