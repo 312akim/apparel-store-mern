be1eb3081ff5bb8d51c4e1589912dca5dfdd3788
Alex Kim <312akim@gmail.com>
2020-09-15 21:42:20 -0500

    Responsive design improvements

--- src/component/Products/ProductItemComponent.js
+++ src/component/Products/ProductItemComponent.js
@@ -1,22 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Button } from 'reactstrap';
 import { Link } from 'react-router-dom'
 import { ItemDisplayCard, ItemDisplayImg, ItemDisplayBody } from './ProductItemStyles';
-
-// const ItemDisplay = ({item}) => {
-//     return (
-//         <Card className="itemCards">
-//             <Link to={`/store/${item.name}`}>
-//                 <CardImg className="productCardImage" top width="20%" src={item.images[0].src} alt={item.name} />
-//             </Link>
-//             <CardBody className="productCardBody">
-//                 <CardTitle className="productCardTitle">{item.name}</CardTitle>
-//                 <CardText className="productCardPrice">${item.price}</CardText>
-//                 <Button className="productCardAddCart">+</Button>
-//             </CardBody>
-//         </Card>
-//         )
-// }
 
 const ItemDisplay = ({item}) => {
     return (
--- src/component/Products/ProductItemStyles.js
+++ src/component/Products/ProductItemStyles.js
@@ -2,12 +2,12 @@ import styles from 'styled-components';
 
 export const ItemDisplayCard = styles.div`
     width: 80%;
-    height: 100px;
+    height: 113px;
     margin: 5% 10%;
     border: solid black 6px;
     background: grey;
 `
-//Need to fix emplty pixel
+//Need to fix empty pixel
 // https://stackoverflow.com/questions/5804256/image-inside-div-has-extra-space-below-the-image
 export const ItemDisplayImg = styles.img`
     display: inline-block;
@@ -15,12 +15,17 @@ export const ItemDisplayImg = styles.img`
     width:  100px;
     height: 100px;
     object-fit: cover;
-    padding-bottom: 5%;
+    padding-bottom: 0;
 `
 
 export const ItemDisplayBody = styles.div`
     display: inline-block;
     width: 50%;
-    padding: 5%;
+    padding: 0;
+
+    @media (min-width: 480px) {
+        display: block;
+        background: red;
+    }
 `
 
