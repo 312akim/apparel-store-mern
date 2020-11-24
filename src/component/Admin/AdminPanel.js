119c73af08138844b7eb738224dc359e861563ac
Alex Kim <312akim@gmail.com>
2020-11-24 14:34:45 -0800

    Admin Page placeholders & css

--- src/component/Admin/AdminPanel.js
+++ src/component/Admin/AdminPanel.js
@@ -1,17 +1,38 @@
 import React from "react";
-import { PanelMerch, PanelOrders } from "./AdminPanelStyles";
+import { AdminMain, PanelMerch, PanelOrders } from "./AdminPanelStyles";
 
-const AdminPanel = () => {
+const AdminPanel = ({products}) => {
+
+    const productList = products.map(item =>{
+        return (
+            <div>
+                <div>
+                    Product: {item.name}
+                </div>
+                <div>
+                    Stock: {item.inventory}
+                </div>
+                <div>EDIT HERE</div>
+                <hr/>
+            </div>
+        )
+    });
 
     return (
-        <div>
+        <AdminMain>
             <PanelMerch>
-                Edit Merchandise Here
+                <h3>
+                    EDIT PRODUCTS
+                </h3>
+                {productList}
             </PanelMerch>
             <PanelOrders>
+                <h3>
+                    Orders
+                </h3>
                 Check Orders Here
             </PanelOrders>
-        </div>
+        </AdminMain>
     )
 }
 
--- src/component/Admin/AdminPanelStyles.js
+++ src/component/Admin/AdminPanelStyles.js
@@ -1,9 +1,27 @@
 import styles from 'styled-components';
 
+export const AdminMain = styles.div`
+    height: 85vh;
+`
+
 export const PanelMerch = styles.div`
-    width: 50%;
+    display: inline-block;
+    padding: 5%;
+    margin: 5%;
+    margin-left: 10%;
+    height: 90%;
+    width: 35%;
+    background: lightgreen;
+    vertical-align: middle;
 `
 
 export const PanelOrders = styles.div`
-    width: 50%;
+    display: inline-block;
+    padding: 5%;
+    margin: 5%;
+    margin-right: 10%;
+    height: 90%;
+    width: 35%;
+    background: lightblue;
+    vertical-align: middle;
 `
