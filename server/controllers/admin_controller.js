1c0e18e48f1505182288f6f04d40c17a100ccf8c
Alex Kim <312akim@gmail.com>
2020-12-13 10:35:31 -0800

    Admin & Products controllers

--- server/controllers/admin_controller.js
+++ server/controllers/admin_controller.js
@@ -1,5 +1,61 @@
-
+const product = require('../models/product');
+const Product = require('../models/product');
+const User = require('../models/user');
 
 module.exports = {
+    // Admin
+    getAdminUsers(req, res) {
+        User.find().exec((err, users) => {
+            if (err) {
+                console.log('Get Admin Users Error---------------', err);
+            }
+            res.status(200).json({users});
+        })
+    },
 
+    // Products
+    createProduct(req, res) {
+        // MongoDB automatically generates id for created data
+        const { name, description, price, quantity } = req.body;
+
+        let newProduct = new Product({
+            name,
+            description,
+            price,
+            quantity
+        })
+
+        newProduct.save();
+        res.status(200).json({product: newProduct});
+    },
+
+    updateProduct(req, res) {
+        const { id } = req.params;
+
+        Product.findById(id).exec((err, product) => {
+            if (err) {
+                console.log('Updated Product-----------------', err);
+            }
+            // Assigning data to req.body
+            product.name = name;
+            product.description = description;
+            product.price = price;
+            product.quantity = quantity;
+            product.save();
+
+            // Send back data for testing
+            res.status(200).json({product});
+        }) 
+    },
+
+    deleteProduct(req, res) {
+        const { id } = req.params;
+
+        Product.deleteOne({_id: id}).exec((err, product) => {
+            if (err) {
+                console.log('Delete One Error---------------', err);
+                res.status(200).json({product});
+            }
+        })
+    }
 };
--- server/controllers/products_controller.js
+++ server/controllers/products_controller.js
@@ -1,5 +1,24 @@
-
+const Product = require('../models/product');
 
 module.exports = {
+    readAllProducts(req, res) {
+    //When using mongoose, use exec method to catch & respond to errors & resolve promise.
+      Product.find({}).exec((err, products) => {
+        if(err) console.log('Get Product Mongoose Error------------------', err);
+        console.log('products-------------', products);
+        res.status(200).json({products});
+      });
+    },
 
-};
+    readProduct(req, res) {
+      //Destructs id from endpoint and assigns it to const id. 1 product
+      const { id } = req.params;
+      //Copy and paste one of the product's id to the url when testing it.
+      //Use the findById to get a specific product.
+      Product.findById(id).exec((err, product) => {
+        if(err) console.log('Get Single Product Error---------------', err);
+        console.log('product--------------', product);
+        res.status(200).json({product});
+      })
+    }
+}
