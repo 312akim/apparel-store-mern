8c7415127ba38ba9f9186451d5fae56a0c5a5006
Alex Kim <312akim@gmail.com>
2021-01-11 18:49:05 -0800

    Static & Floating email footer responsive design

--- src/component/presentational/Footer/FooterComponent.js
+++ src/component/presentational/Footer/FooterComponent.js
@@ -1,23 +1,31 @@
 import React from 'react'
-import { FooterContainer, FooterEmailSignupContainer, EmailSignupText, EmailSignupExit, EmailSignupInput, FooterSocialIconsContainer, FooterNavigationContainer, FooterCopyrightContainer } from './FooterComponentStyles';
+import { MainFooterContainer, StaticEmailSignupContainer, FooterEmailSignupContainer, EmailSignupText, EmailSignupExit, EmailSignupInput, EmailSignupButton, FooterSocialIconsContainer, FooterNavigationContainer, FooterCopyrightContainer, IconContainer, LeftFooterContainer, RightFooterContainer } from './FooterComponentStyles';
 
 
 //Sticky footer? Email subscribe should always be visible...
 const Footer = ({showEmailSignup, footerExitHandler}) => {
 	
 	const EmailSignup = () => {
+		return (
+			<>
+				<EmailSignupText>
+					Stay up to date!
+				</EmailSignupText>
+				<EmailSignupInput placeholder="Email Address">
+				</EmailSignupInput>
+				<EmailSignupButton type="submit">
+					Submit
+				</EmailSignupButton>
+			</>
+		)
+	}
+	// Needs to be resized & centered due to exit button
+	const FloatingEmailSignup = () => {
 		if (showEmailSignup) {
 			return (
 				<FooterEmailSignupContainer className="fixed-bottom">
 					<EmailSignupExit onClick={footerExitHandler}>X</EmailSignupExit>
-					<EmailSignupText>
-						Stay up to date!
-					</EmailSignupText>
-					<EmailSignupInput>
-					</EmailSignupInput>
-					<button type="submit">
-						Submit
-					</button>
+					<EmailSignup />
 				</FooterEmailSignupContainer>
 			)
 		} else {
@@ -27,21 +35,56 @@ const Footer = ({showEmailSignup, footerExitHandler}) => {
 		}
 	}
 
+	const SocialIconComponent = () => {
+		return (
+			<FooterSocialIconsContainer>
+				<IconContainer>
+					<i class="fa fa-instagram fa-2x"></i>
+				</IconContainer>
+				<IconContainer>
+					<i class="fa fa-youtube-square fa-2x"></i>
+				</IconContainer>
+				<IconContainer>
+					<i class="fa fa-facebook-square fa-2x"></i>
+				</IconContainer>
+				<IconContainer>
+					<i class="fa fa-twitter-square fa-2x"></i>
+				</IconContainer>
+			</FooterSocialIconsContainer>
+		)
+	}
+
+	const CopyrightComponent = () => {
+		return (
+			<FooterCopyrightContainer>
+				© 2020 Alex, Inc. All Rights Reserved
+			</FooterCopyrightContainer>
+		)
+	}
+
 	return (
 		<>
-			<FooterContainer>
-				<FooterSocialIconsContainer>
-					Social Icons
-				</FooterSocialIconsContainer>
-				<FooterNavigationContainer>
-					<div>Navigation</div>
-				</FooterNavigationContainer>
-
-				<FooterCopyrightContainer>
-					Copyright
-				</FooterCopyrightContainer>
-			</FooterContainer>
-			<EmailSignup />
+			<MainFooterContainer>
+				<LeftFooterContainer>
+					<SocialIconComponent/>
+					<StaticEmailSignupContainer>
+						<EmailSignup/>
+					</StaticEmailSignupContainer>
+				</LeftFooterContainer>
+				<RightFooterContainer>
+					<FooterNavigationContainer>
+						<div>Navigation</div>
+						<div>Store</div>
+						<div>About Us</div>
+						<div>Contact Us</div>
+						<div>Brand 1</div>
+						<div>Brand 2</div>
+					</FooterNavigationContainer>
+				</RightFooterContainer>
+				<CopyrightComponent/>
+			</MainFooterContainer>
+			<FloatingEmailSignup />
+			
 		</>
 	)
 }
--- src/component/presentational/Footer/FooterComponentStyles.js
+++ src/component/presentational/Footer/FooterComponentStyles.js
@@ -1,21 +1,41 @@
 import styles from 'styled-components';
 import COLORS from '../../../shared/colorScheme';
 
-export const FooterContainer = styles.div`
+export const MainFooterContainer = styles.div`
+    position: relative;
+    z-index: 10;
     background: ${COLORS.primaryHighlight};
     color: ${COLORS.secondaryLight};
-    height: 19vh;
-    padding-top: 0.5vh;
+    height: 30vh;
+    padding-top:2vh;
     padding-bottom: 9vh;
+`
+export const StaticEmailSignupContainer = styles.div`
+    width: 100%;
+    margin-top: 2vh;
+
+    @media (min-width: 768px) {
+        padding: 0 15%;
+    }
+
+    @media (min-width: 992px) {
+        padding: 0 20%;
+    }
+
+    @media (min-width: 1200px) {
+        padding: 0 30%;
+    }
 `
 
 export const FooterEmailSignupContainer = styles.div`
     text-align: center;
-    height: 9vh;
+    height: 15vh;
     width: 50%;
     margin-left: 25%;
     background: ${COLORS.primaryHighlight};
     color: ${COLORS.secondaryLight};
+    bottom: 1vh;
+    z-index: 5;
 
     @media (min-width: 992px) {
         margin-left: 30%;
@@ -28,31 +48,66 @@ export const FooterEmailSignupContainer = styles.div`
     }
 `
 
-export const EmailSignupText = styles.div`
-
-`
-
 export const EmailSignupExit = styles.button`
-
+    display: inline-block;
+    float: right;
 `
+
+export const EmailSignupText = styles.div`
+    display: inline-block;
+`
+
 
 export const EmailSignupInput = styles.input`
-    width: 90%;
+    width: 80%;
+    margin-top: 0.5rem;
+`
+
+export const EmailSignupButton = styles.button`
+    margin-top: 0.5rem;
 `
 
 export const FooterSocialIconsContainer = styles.div`
     display: inline-block;
-    height: 80%;
-    width: 33%;
+    height: 50%;
+    width: 100%;
 `
 
 export const FooterNavigationContainer = styles.div`
     display: inline-block;
-    height: 80%;
-    width: 33%;
+    height: 100%;
+    width: 50%;
 `
 
 export const FooterCopyrightContainer = styles.div`
     height: 20%;
-    padding-top: 1vh;
+    padding-top: 2vh;
+    background: ${COLORS.primaryHighlight};
+`
+
+export const IconContainer = styles.div`
+    display: inline-block;
+    padding: 0 2%;
+`
+
+export const LeftFooterContainer = styles.div`
+    display: inline-block;
+    width: 50%;
+    padding-left: 10%;
+
+    @media (min-width: 768px) {
+        padding-left: 15%;
+    }
+`
+
+export const RightFooterContainer = styles.div`
+    display: inline-block;
+    vertical-align: top;
+    height: 100%;
+    width: 50%;
+    padding-right: 10%;
+
+    @media (min-width: 768px) {
+        padding-right: 15%;
+    }
 `
