031b495c16add9548f64531ee8f119cf6a761f50
Alex Kim <312akim@gmail.com>
2021-02-23 18:46:02 -0800

    snipcart webcrawler test2, set item url to full length

--- src/component/presentational/Products/ProductItemPageComponent.js
+++ src/component/presentational/Products/ProductItemPageComponent.js
@@ -11,7 +11,7 @@ class ProductPage extends Component {
 		this.state = {
 			displayedImage: props.item.images[0].src,
             productQuantity: 1,
-            productSize: 'Choose Size'
+            productSize: 'X-Small'
 		}
     }
 
@@ -46,7 +46,7 @@ class ProductPage extends Component {
                     <StyledButton largeWidth='70%' className="snipcart-add-item"
                       data-item-id={this.props.item._id}
                       data-item-price={this.props.item.price}
-                      data-item-url="/store/Everstar%20T-Shirt"
+                      data-item-url="https://apparel-store-mern.netlify.app/store/Everstar%20T-Shirt"
                       data-item-description={this.props.item.description}
                       data-item-image={this.props.item.images[0].src}
                       data-item-name={this.props.item.name}
